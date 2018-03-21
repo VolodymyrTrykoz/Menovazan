f460c399760bcf71334e33a28e3f6e9286b3ac35
Volodymyr Trykoz <volodymyrtz@gmail.com>
2018-03-21 18:57:41 +0200

    animation changes

--- app/js/main-js.js
+++ app/js/main-js.js
@@ -8,10 +8,18 @@ $(document).ready(function() {
 
 
 
-    $('.btn--instr').mouseover(function(){
-        console.log ($('.btn--instr span'))
-        $('.btn--instr span').fadeIn("slow")
-    })
+    $('.btn--instr').hover(function(){
+        $('.btn--instr span').fadeIn("slow");
+        TweenMax.to(('.btn--instr span'), 0.5, {x:"18px"}, delay: 0.5);
+
+    });
+    $('.btn--instr').mouseleave(function(){
+        $('.btn--instr span').fadeOut("fast");
+
+    });
+
+
+
 
 
 
